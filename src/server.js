a5b7d757db13d16de3167e86344921a11bad5273
fdhhhdjd <nguyentientai10@gmail.com>
2024-12-01 16:59:17 +0700

    feat: add morgan and use lib express

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
+// REQUIRE
 const appConfig = require("./app/share/configs/app.conf");
 const app = require("./app/app");
 
 const PORT = appConfig.Port;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running http://localhost:${PORT}`);
 });
