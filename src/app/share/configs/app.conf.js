2555cd947d460db3f2d7c21ca340216422e0da74
fdhhhdjd <nguyentientai10@gmail.com>
2024-12-01 16:39:49 +0700

    feat: run code node

---
+++ src/app/share/configs/app.conf.js
@@ -0,0 +1,8 @@
+const appConstants = require("../constants/app.constants");
+
+const appConfig = {
+  Port: appConstants.Port,
+  NodeEnv: appConstants.NodeEnv,
+};
+
+module.exports = appConfig;
---
+++ src/app/share/constants/app.constants.js
@@ -0,0 +1,6 @@
+const appConstants = {
+  Port: process.env.PORT || 5000,
+  NodeEnv: process.env.NODE_ENV || "development",
+};
+
+module.exports = appConstants;
